2f90f9ad93c5b01c109dd6584ffbb48730e74ce1
TimHu950112 <tim20060112@gmail.com>
2023-01-01 14:50:23 +0800

    4
    4

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import string
 import time
 #初始化資料庫連線
 client=pymongo.MongoClient("mongodb+srv://root:root123@cluster0.rpebx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",tlsCAFile=certifi.where())
-db=client.member_system
+db=client.kofu_member_system
 print("\n"+"資料庫連線成功".center(87,"*")+"\n")
 
 #初始化 flask 伺服器
@@ -26,6 +26,7 @@ def index():
     session["status"]=None
     return render_template("login.html")
 
+
 @app.route("/member",methods=["GET","POST"])
 def member():
     if "nickname" in session:
@@ -33,17 +34,109 @@ def member():
         email=session["email"]
         password=session["password"]
         return render_template("member.html",nickname=nickname,email=email,password=password)
-    return redirect("/")
+    flash("請先登入")
+    return render_template("login.html")
+
+@app.route("/function")
+def function():
+    if "nickname" in session:
+        return render_template("function.html")
+    flash("請先登入")
+    return render_template("login.html")
+
+@app.route("/add_page")
+def add_page():
+    if "nickname" in session:
+        nickname=session["nickname"]
+        return render_template("check_old.html",nickname=nickname)
+    flash("請先登入")
+    return render_template("login.html")
+
+# @app.route("/add",methods=["GET","POST"])
+# def add():
+#     session["member_name"]=request.form["member_name"]
+#     session["phone"]=request.form["phone"]
+#     if request.form.get("forever")=="forever":
+#         session["forever"]="forever"
+#     else:
+#         session["forever"]="not"
+
+#     collection=db.user
+#     collection.insert_one({
+#     "member_name":session["member_name"],
+#     "phone":session["phone"],
+#     "forever":session["forever"]})
+#     return redirect("/add_page")
+
+@app.route("/add",methods=["GET","POST"])
+def add():
+    session["member_name"]=request.form["member_name"]
+    session["phone"]=request.form["phone"]
+    if request.form.get("forever")=="forever":
+        session["forever"]="forever"
+    else:
+        session["forever"]="not"
+
+    collection=db.new
+    collection.insert_one({
+    "member_name":session["member_name"],
+    "phone":session["phone"],
+    "forever":session["forever"]})
+    flash("會員註冊成功")
+    return render_template("check_old.html")
+
+@app.route("/check_old",methods=["GET","POST"])
+def check_old():
+    session["phone"]=request.form["phone"]
+    
+    collection=db.user
+    result=collection.find_one({
+        "phone":session["phone"]
+    })
+
+    collection=db.new
+    result_one=collection.find_one({
+        "phone":session["phone"]
+    })
+
+    if result_one==None:
+        if result==None:
+            print("none")
+            return render_template("add_new.html",phone=session["phone"],member_name="會員名稱")
+        else:
+            session["member_name"]=result["member_name"]
+            flash("舊會員"+result["member_name"]+"請確認是否註冊為永久會員")
+            return render_template("add_new.html",phone=session["phone"],member_name=session["member_name"])
+    else:
+        flash("已是新會員")
+        return render_template("check_old.html")
+
+
+@app.route("/search",methods=["GET","POST"])
+def search():
+    session["phone"]=request.form["phone"]
+    collection=db.new
+    result=collection.find_one({"phone":session["phone"]})
+    if result==None:
+        flash("查無資料")
+        return render_template("function.html",status="無資料")
+    else:
+        return render_template("function.html",status=result["member_name"])
+
+
+
 
 @app.route("/error")
 def error():
     message=request.args.get("msg","發生錯誤，請聯繫客服")
     return render_template("error.html",message=message)
 
+
 @app.route("/signup_page")
 def signup_page():
     session["status"]=None
     return render_template("signup.html")
+
 
 @app.route("/signup",methods=["GET","POST"])
 def signup():
@@ -72,12 +165,15 @@ def check():
     if session["status"]=="forget_twice":
         emails=session["email"]
     else:
-        emails=session["email"]
+        emails="tim20060112@gmail.com"
         nickname=session["nickname"]
     import email.message
     msg=email.message.EmailMessage()
     msg["From"]="eemailcheck9@gmail.com"
-    msg["To"]=session["email"]
+    if session["status"]=="forget_twice":
+        msg["To"]=session["email"]
+    else:
+        msg["To"]=emails
     msg["Subject"]="Email驗證"
 
     msg.add_alternative("<h3>會員系統驗證碼</h3>您的驗證碼為:"+key+" （請勿向他人傳送此驗證碼，驗證碼限時5分鐘）",subtype="html") #HTML信件內容
@@ -99,7 +195,7 @@ def check():
     if session["status"]=="forget_twice":
         return render_template("check.html",emails=emails)
     else:
-        return render_template("check.html",nickname=nickname,emails=emails)
+        return render_template("check.html",nickname=nickname,emails="後台信箱")
     
 
 
@@ -149,11 +245,12 @@ def login():
     session["nickname"]=result["nickname"]
     session["email"]=result["email"]
     session["password"]=result["password"]
-    return redirect("/member")
+    return redirect("/function")
 
 @app.route("/logout")
 def logout():
     del session["nickname"]
+    flash("登出成功")
     return redirect("/")
 
 @app.route("/change",methods=["GET","POST"])
